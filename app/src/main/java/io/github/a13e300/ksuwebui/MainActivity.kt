4e5b04e3006941291a7889f2353c60d18a0c53d1
lamprose <2316892579@qq.com>
2024-11-07 15:24:22 +0800

    Add `action.sh` support

--- app/src/main/java/io/github/a13e300/ksuwebui/MainActivity.kt
+++ app/src/main/java/io/github/a13e300/ksuwebui/MainActivity.kt
@@ -7,17 +7,23 @@ import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.core.view.updatePadding
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.topjohnwu.superuser.nio.FileSystemManager
 import io.github.a13e300.ksuwebui.databinding.ActivityMainBinding
 import io.github.a13e300.ksuwebui.databinding.ItemModuleBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @SuppressLint("NotifyDataSetChanged")
 class MainActivity : AppCompatActivity(), FileSystemService.Listener {
@@ -44,14 +50,22 @@ class MainActivity : AppCompatActivity(), FileSystemService.Listener {
             val cutoutAndBars = insets.getInsets(
                 WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
             )
-            v.updatePadding(left = cutoutAndBars.left, top = cutoutAndBars.top, right = cutoutAndBars.right)
+            v.updatePadding(
+                left = cutoutAndBars.left,
+                top = cutoutAndBars.top,
+                right = cutoutAndBars.right
+            )
             return@setOnApplyWindowInsetsListener insets
         }
         ViewCompat.setOnApplyWindowInsetsListener(binding.list) { v, insets ->
             val cutoutAndBars = insets.getInsets(
                 WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
             )
-            v.updatePadding(left = cutoutAndBars.left, bottom = cutoutAndBars.bottom, right = cutoutAndBars.right)
+            v.updatePadding(
+                left = cutoutAndBars.left,
+                bottom = cutoutAndBars.bottom,
+                right = cutoutAndBars.right
+            )
             return@setOnApplyWindowInsetsListener insets
         }
 
@@ -102,7 +116,9 @@ class MainActivity : AppCompatActivity(), FileSystemService.Listener {
             val showDisabled = prefs.getBoolean("show_disabled", false)
             fs.getFile("/data/adb/modules").listFiles()!!.forEach { f ->
                 if (!f.isDirectory) return@forEach
-                if (!fs.getFile(f, "webroot").isDirectory) return@forEach
+                if (!fs.getFile(f, "webroot").isDirectory &&
+                    !fs.getFile(f, "action.sh").exists()
+                ) return@forEach
                 if (fs.getFile(f, "disable").exists() && !showDisabled) return@forEach
                 var name = f.name
                 val id = f.name
@@ -121,7 +137,17 @@ class MainActivity : AppCompatActivity(), FileSystemService.Listener {
 
                     }
                 }
-                mods.add(Module(name, id, desc, author, version))
+                mods.add(
+                    Module(
+                        name,
+                        id,
+                        desc,
+                        author,
+                        version,
+                        fs.getFile(f, "webroot").isDirectory,
+                        fs.getFile(f, "action.sh").exists()
+                    )
+                )
             }
             runOnUiThread {
                 moduleList = mods
@@ -145,7 +171,15 @@ class MainActivity : AppCompatActivity(), FileSystemService.Listener {
         binding.swipeRefresh.isRefreshing = false
     }
 
-    data class Module(val name: String, val id: String, val desc: String, val author: String, val version: String)
+    data class Module(
+        val name: String,
+        val id: String,
+        val desc: String,
+        val author: String,
+        val version: String,
+        val hasWebUi: Boolean = false,
+        val hasAction: Boolean = false
+    )
 
     class ViewHolder(val binding: ItemModuleBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -169,14 +203,50 @@ class MainActivity : AppCompatActivity(), FileSystemService.Listener {
             holder.binding.author.text = resources.getString(R.string.author, item.author)
             holder.binding.version.text = resources.getString(R.string.version, item.version)
             holder.binding.desc.text = item.desc
-            holder.binding.root.setOnClickListener {
-                startActivity(
-                    Intent(this@MainActivity, WebUIActivity::class.java)
-                        .setData(Uri.parse("ksuwebui://webui/$id"))
-                        .putExtra("id", id)
-                        .putExtra("name", name)
-                )
+            if (item.hasWebUi) {
+                holder.binding.openWebUi.apply {
+                    visibility = View.VISIBLE
+                    setOnClickListener {
+                        startActivity(
+                            Intent(this@MainActivity, WebUIActivity::class.java)
+                                .setData(Uri.parse("ksuwebui://webui/$id"))
+                                .putExtra("id", id)
+                                .putExtra("name", name)
+                        )
+                    }
+                }
             }
+            if (item.hasAction) {
+                holder.binding.action.apply {
+                    visibility = View.VISIBLE
+
+                    val logContent = StringBuilder()
+                    setOnClickListener {
+                        var dialog: AlertDialog? = null
+                        runOnUiThread {
+                            dialog = AlertDialog.Builder(this@MainActivity)
+                                .setTitle(R.string.execution_results)
+                                .setMessage(R.string.executing)
+                                .show()
+                        }
+
+                        logContent.clear()
+                        lifecycleScope.launch(Dispatchers.IO) {
+                            val result = withNewRootShell(true) {
+                                newJob().add("sh /data/adb/modules/${item.id}/action.sh")
+                                    .to(ArrayList(), ArrayList()).exec()
+                            }
+
+                            val stdout = result.out.joinToString(separator = "\n")
+                            withContext(Dispatchers.Main) {
+                                dialog?.setMessage(stdout)
+                            }
+                        }
+
+                    }
+                }
+            }
+
         }
 
     }
